dde45f9c30696e448891dabd663cb72626617bb6
dyutinone <40772716+dyutinone@users.noreply.github.com>
2019-12-28 17:09:22 +0300

    +

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
 class Server:
-    def __init__(self, token, groupID, databaseName):
+    def __init__(self, token, groupID):
         self.token = token
         self.groupID = groupID
 
@@ -15,8 +15,7 @@ class Server:
         return vk_session, vk, longpoll
             
 server = Server(
-    token="70cb1f8026e1adf42d7e9534edab8c8ca8e6c0796f23fa71ab338f196b7f2d853182b179d13c97f179acf",
-    groupID="186661962",
-    databaseName="usersDB.db",
+    token=token,
+    groupID=groupID,
 )
 vk_session, vk, longpoll = server.connectToVKApi()
