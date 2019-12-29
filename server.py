6f09cd114d804b7bdc2acd14571437179ddda7bb
dyutinone <40772716+dyutinone@users.noreply.github.com>
2019-12-29 11:46:32 +0300

    +

--- server.py
+++ server.py
@@ -15,8 +15,8 @@ class Server:
         return vk_session, vk, longpoll
     
 server = Server(
-    token="70cb1f8026e1adf42d7e9534edab8c8ca8e6c0796f23fa71ab338f196b7f2d853182b179d13c97f179acf",
-    groupID="186661962",
+    token="your_token",
+    groupID="your_group_ID",
 )
 vk_session, vk, longpoll = server.connectToVKApi()
 
